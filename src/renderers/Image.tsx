d3e7a4b502ea202c5d6fc1caf962669a3da727f9
Dmytro Sapozhnyk <dimonsuper010@gmail.com>
2021-07-30 12:06:37 +0300

    Added build

--- src/renderers/Image.tsx
+++ src/renderers/Image.tsx
@@ -3,9 +3,13 @@ import Spinner from '../components/Spinner';
 import { Renderer, Tester } from './../interfaces';
 import WithHeader from './wrappers/withHeader';
 import WithSeeMore from './wrappers/withSeeMore';
+import GlobalContext from '../context/Global';
+import { GlobalCtx } from "../interfaces";
 
-export const renderer: Renderer = ({ story, action, isPaused, config }) => {
+export const renderer: Renderer = ({ story, action, config }) => {
     const [loaded, setLoaded] = React.useState(false);
+    const { isPaused } = React.useContext<GlobalCtx>(GlobalContext);
+
     const { width, height, loader, storyStyles } = config;
     let computedStyles = {
         ...styles.storyContent,
